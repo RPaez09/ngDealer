8a58a038167f01e6ab50f3dd899c4623843dd348
Richard Paez <Richardp809@gmail.com>
2017-05-22 21:43:21 -0400

    Adding scaffolding for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+const routes: Routes = [  { path: '', redirectTo: 'home', pathMatch: 'full' },
+                          { path: 'home', component: HomeComponent },
+                          { path: 'about', component: AboutComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
