4086a5fa871ca11c6573ae3aa27c06b89e5e3ad2
Richard Paez <Richardp809@gmail.com>
2017-05-28 23:06:15 -0400

    Styling fixes and addition of an admin panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { CarBoxComponent } from './components/car-box/car-box.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 
 const routes: Routes = [  { path: '', redirectTo: 'home', pathMatch: 'full' },
                           { path: 'home', component: HomeComponent },
-                          { path: 'about', component: AboutComponent }];
+                          { path: 'about', component: AboutComponent },
+                          { path: 'admin', component: AdminComponent }];
 
 @NgModule({
   declarations: [
     AppComponent, 
     HomeComponent,
     AboutComponent,
-    CarBoxComponent
+    CarBoxComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
