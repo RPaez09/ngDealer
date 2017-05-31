c056d346fe986c0c07c13b7bed9521d93bb0f307
Richard Paez <Richardp809@gmail.com>
2017-05-30 22:09:00 -0400

    Car table now shows car ID

--- src/app/models/Car.model.ts
+++ src/app/models/Car.model.ts
@@ -3,12 +3,14 @@ export class Car {
     model: string;
     year: number;
     price: number;
+    _id: string;
 
-    constructor( { "make": make, "model": model, "year": year, "price": price } ) {
+    constructor( { "make": make, "model": model, "year": year, "price": price, "_id": _id } ) {
         this.make = make;
         this.model = model;
         this.year = year;
         this.price = price;
+        this._id = _id;
     }
 
     public changePrice(newPrice):void {
