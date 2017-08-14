4018e39a1e24baeece7d70096f6e9278c344a4ae
Richard Paez <Richardp809@gmail.com>
2017-08-14 12:03:52 -0400

    Epanded car model to add more information on a vehicle.

--- src/app/models/Car.model.ts
+++ src/app/models/Car.model.ts
@@ -3,13 +3,29 @@ export class Car {
     model: string;
     year: number;
     price: number;
+    mileage: number;
+    color: string;
+    trim: string;
+    hidden: boolean;
     _id: string;
 
-    constructor( { "make": make, "model": model, "year": year, "price": price, "_id": _id } ) {
+    constructor( {  "make": make, 
+                    "model": model, 
+                    "year": year, 
+                    "price": price,
+                    "mileage": mileage,
+                    "color": color,
+                    "trim":trim,
+                    "hidden": hidden,
+                    "_id": _id } ) {
         this.make = make;
         this.model = model;
         this.year = year;
         this.price = price;
+        this.mileage = mileage;
+        this.color = color;
+        this.trim = trim;
+        this.hidden = hidden;
         this._id = _id;
     }
 
