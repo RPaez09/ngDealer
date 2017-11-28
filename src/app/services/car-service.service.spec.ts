0a1e84699c64be0e2da336ecaf46e18f2697cbee
Richard Paez <Richardp809@gmail.com>
2017-11-28 18:57:23 -0500

    Beginning testing for car-service

--- src/app/services/car-service.service.spec.ts
+++ src/app/services/car-service.service.spec.ts
@@ -1,15 +1,40 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { 
+  TestBed, 
+  inject,
+  fakeAsync,
+  tick } from '@angular/core/testing';
 
-import { CarServiceService } from './car-service.service';
+import { MockBackend } from '@angular/http/testing';
 
-describe('CarServiceService', () => {
+import { 
+  Http,
+  ConnectionBackend,
+  BaseRequestOptions,
+  Response,
+  ResponseOptions } from '@angular/http';
+
+  import { CarService } from './car-service.service';
+
+describe('CarService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CarServiceService]
+      providers: 
+      [ 
+        BaseRequestOptions, 
+        MockBackend, 
+        CarService,
+        { provide: Http,
+          useFactory: (
+            backend: ConnectionBackend,
+            defaultOptions: BaseRequestOptions ) => {
+              return new Http(backend , defaultOptions);
+            }, deps : [ MockBackend, BaseRequestOptions ] 
+        }
+      ]
     });
   });
 
-  it('should be created', inject([CarServiceService], (service: CarServiceService) => {
+  it('should be created', inject([CarService], (service: CarService) => {
     expect(service).toBeTruthy();
   }));
 });
