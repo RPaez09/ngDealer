57c04be84ab9d6d36c98f50ec1911c4397bdf1ed
Richard Paez <Richardp809@gmail.com>
2017-12-07 18:28:28 -0500

    user actions created

---
+++ src/app/user-actions.ts
@@ -0,0 +1,72 @@
+import { Action } from '@ngrx/store';
+import { User } from 'app/models/user.model';
+
+export const GETAUSER = "[Users] Get a user";
+export const GETAUSERSUCCESS = "[Users] Get a user success";
+
+export const CREATAEUSER = "[Users] Create a user";
+export const CREATEAUSERSUCCESS = "[Users] Create a user success";
+
+export const SIGNIN = "[Users] Sign in a user";
+export const SIGNINSUCCESS = "[Users] Sign in a user success";
+
+export const SIGNOUT = "[Users] Sign out a user";
+export const SIGNOUTSUCCESS = "[Users] Sign out a user";
+
+export class GetAUser implements Action {
+    readonly type = GETAUSER;
+
+    constructor( ){ }
+}
+
+export class GetAUserSuccess implements Action {
+    readonly type = GETAUSERSUCCESS;
+
+    constructor( public payload: User ){ }
+}
+
+export class CreateAUser implements Action {
+    readonly type = CREATAEUSER;
+
+    constructor( ){ }
+}
+
+export class CreateAUserSuccess implements Action {
+    readonly type = CREATEAUSERSUCCESS;
+
+    constructor( public payload: User ){ }
+}
+
+export class SignIn implements Action {
+    readonly type = SIGNIN;
+
+    constructor( ){ }
+}
+
+export class SignInSuccess implements Action {
+    readonly type = SIGNINSUCCESS;
+
+    constructor( public payload: User ){ }
+}
+
+export class SignOut implements Action {
+    readonly type = SIGNOUT;
+
+    constructor( ){ }
+}
+
+export class SignOutSuccess implements Action {
+    readonly type = SIGNOUTSUCCESS;
+
+    constructor( public payload: User ){ }
+}
+
+export type All
+    = GetAUser
+    | GetAUserSuccess
+    | CreateAUser
+    | CreateAUserSuccess
+    | SignIn
+    | SignInSuccess
+    | SignOut
+    | SignOutSuccess;
