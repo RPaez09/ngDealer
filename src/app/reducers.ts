961819f465c6d25c8df56e8ab31c3cb58f7c377b
Richard Paez <Richardp809@gmail.com>
2017-12-07 19:29:16 -0500

    expanded to include selecting users

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -1,11 +1,14 @@
 import * as fromCars from 'app/car-reducer';
+import * as fromUsers from 'app/user-reducer';
 
 export interface State {
-    cars: fromCars.State;
+    cars: fromCars.State,
+    users: fromUsers.State
 }
 
 export const reducers = {
-    cars: fromCars.reducer
+    cars: fromCars.reducer,
+    users: fromUsers.reducer
 };
 
 export function selectCars( state: State ) {
@@ -14,4 +17,16 @@ export function selectCars( state: State ) {
 
 export function selectCar( state: State ) {
     return state.cars.SelectedCar
+}
+
+export function selectCurrentUser( state: State ){
+    return state.users.currentUser
+}
+
+export function selectDetailUser( state: State ){
+    return state.users.detailUser
+}
+
+export function selectJWTtoken( state: State ){
+    return state.users.token
 }
