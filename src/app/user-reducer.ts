cbc363eaf9b650374b09bae8e7f2aced48bc1f7d
Richard Paez <Richardp809@gmail.com>
2017-12-07 19:20:59 -0500

    created a user-reducer

---
+++ src/app/user-reducer.ts
@@ -0,0 +1,53 @@
+import { User } from 'app/models/User.model';
+import * as UserActions from 'app/user-actions';
+
+export interface State {
+    currentUser : User,
+    detailedUser : User ,
+    token : String
+}
+
+const initialState: State {
+    currentUser: null,
+    detailedUser: null,
+    token : null
+}
+
+export function reducer( state = initialState, action: UserActions.All ): State {
+    switch ( action.type ) {
+
+        case UserActions.GETAUSER: {
+            return state;
+        }
+
+        case UserActions.GETAUSERSUCCESS: {
+            return { ...state , detailedUser: action.payload };
+        }
+
+        case UserActions.CREATEAEUSER: {
+            return state;
+        }
+
+        case UserActions.CREATEAUSERSUCCESS: {
+            return state;
+        }
+
+        case UserActions.SIGNIN: {
+            return state;
+        }
+
+        case UserActions.SIGNINSUCCESS: {
+            return { ...state , currentUser: action.payload.user , token: action.payload.token };
+        }
+
+        case UserActions.SIGNOUT: {
+            return state;
+        }
+
+        case UserActions.SIGNOUTSUCCESS: {
+            return { ...state , currentUser: null , token: null }
+        }
+
+        default: { return state; }
+    }
+}
